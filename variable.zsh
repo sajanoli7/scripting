15d336bab91bf307db44948371b8ffefbb391ed0
sajanoli92 <sajanoli92@gmail.com>
2024-09-11 21:54:27 +0545

    array

--- variable.zsh
+++ variable.zsh
@@ -15,3 +15,17 @@ echo "My college name is $COLLEGE"
 COLLEGE="kalimati"
 echo "my college name is $COLLEGE"
 
+#ARRAY
+#How to define an array?
+#myArray=( 1 2 Hello "Hey man")
+
+#How to get values from an array?
+#echo "${myArray[0]}"
+#echo "${myArray[1]}"
+myArray=( 1 20 30.5 Hello "Hey Buddy!")
+echo "${myArray[3]}"
+myArray[3]="rambo boy"
+echo "${myArray[3]}"
+
+echo "All the values in array are ${myArray[*]}"
+
